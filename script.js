3f30cee7bd7b8d26ff3fc6cee00c2a0608c4fba0
xuanntk <xuanntk2003@gmail.com>
2024-07-10 22:30:34 +0700

    fix

--- script.js
+++ script.js
@@ -26,3 +26,47 @@ seekBar.oninput = function() {
     const seekTo = (seekBar.value / 100) * audio.duration;
     audio.currentTime = seekTo;
 }
+
+// phần chuyển bài hát
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.getElementById("links");
+  const coban = document.getElementById("coban");
+  const totnghiep = document.getElementById("totnghiep");
+
+  function showContent(id) {
+    links.classList.add("hidden");
+    coban.classList.add("hidden");
+    totnghiep.classList.add("hidden");
+
+    document.getElementById(id).classList.remove("hidden");
+  }
+
+  if (window.location.hash) {
+    const hash = window.location.hash.substring(1);
+    if (hash === "coban" || hash === "totnghiep") {
+      showContent(hash);
+    }
+  }
+
+  document
+    .getElementById("showCoban")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      window.location.hash = "coban";
+      showContent("coban");
+    });
+
+  document
+    .getElementById("showTotnghiep")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      window.location.hash = "totnghiep";
+      showContent("totnghiep");
+    });
+});
+
+
+
+
+
+
